19eb3eb4513573273dfbd597f63d924f682fb49d
lebin25 <lebin642@gmail.com>
2023-10-19 20:43:56 +0700

    UI/Layout Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Forgotpassword from './pages/Forgotpassword';
+import MainLayout from './components/MainLayout';
+import Dashboard from './pages/Dashboard';
+import Enquiries from './pages/Enquiries';
+import Bloglist from './pages/Bloglist';
+import Blogcatlist from './pages/Blogcatlist';
+import Orders from './pages/Orders';
+import Customers from './pages/Customers';
+import Colorlist from './pages/Colorlist';
+import Categorylist from './pages/Categorylist';
+import Brandlist from './pages/Brandlist';
+import Productlist from './pages/Productlist';
+import Addblog from './pages/Addblog';
+import Addblogcat from './pages/Addblogcat';
+import Addcolor from './pages/Addcolor';
+import Addcat from './pages/Addcat';
+import Addbrand from './pages/Addbrand';
+import Addproduct from './pages/Addproduct';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Router>
+         <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/reset-password' element={<Resetpassword />} />
+            <Route path='/forgot-password' element={<Forgotpassword />} />
+            <Route path='/admin' element={<MainLayout />}>
+               <Route index element={<Dashboard />} />
+               <Route path='enquiries' element={<Enquiries />} />
+               <Route path='blog' element={<Addblog />} />
+               <Route path='list-blog' element={<Bloglist />} />
+               <Route path='list-blog-category' element={<Blogcatlist />} />
+               <Route path='blog-category' element={<Addblogcat />} />
+               <Route path='orders' element={<Orders />} />
+               <Route path='customers' element={<Customers />} />
+               <Route path='color' element={<Addcolor />} />
+               <Route path='list-color' element={<Colorlist />} />
+               <Route path='category' element={<Addcat />} />
+               <Route path='list-category' element={<Categorylist />} />
+               <Route path='brand' element={<Addbrand />} />
+               <Route path='list-brand' element={<Brandlist />} />
+               <Route path='product' element={<Addproduct />} />
+               <Route path='list-product' element={<Productlist />} />
+            </Route>
+         </Routes>
+      </Router>
+   );
 }
 
 export default App;
