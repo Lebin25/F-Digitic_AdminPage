20f9a4f41be9bee3e4299c22e7364066aa47a52e
Tuan Kiet <ltk23102002@gmail.com>
2023-10-30 21:52:21 +0700

    kiet/updateBlogCategory-deleteBlogCategory Done

--- src/features/bcategory/bcategoryService.js
+++ src/features/bcategory/bcategoryService.js
@@ -10,11 +10,33 @@ const getBlogCategories = async () => {
 const createBlogCategory = async (bcat) => {
     const response = await axios.post(`${base_url}blogcategory/`, bcat, config)
     return response.data;
- }
+}
+
+const updateBlogCategory = async (blogCat) => {
+    const response = await axios.put(
+        `${base_url}blogcategory/${blogCat.id}`,
+        { title: blogCat.blogCatData.title },
+        config
+    )
+    return response.data;
+}
+
+const getBlogCategory = async (id) => {
+    const response = await axios.get(`${base_url}blogcategory/${id}`, config)
+    return response.data;
+}
+
+const deleteBlogCategory = async (id) => {
+    const response = await axios.delete(`${base_url}blogcategory/${id}`, config)
+    return response.data;
+}
 
 const bCategoryService = {
     getBlogCategories,
     createBlogCategory,
+    getBlogCategory,
+    updateBlogCategory,
+    deleteBlogCategory
 }
 
 export default bCategoryService
