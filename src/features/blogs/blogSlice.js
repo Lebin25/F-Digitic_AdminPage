8a20ef17ca89c59bff5063ca46e74012d71c37bc
lebin25 <lebin642@gmail.com>
2023-10-30 17:41:49 +0700

    Fix Toast Add Success

--- src/features/blogs/blogSlice.js
+++ src/features/blogs/blogSlice.js
@@ -1,68 +1,71 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, createAction } from '@reduxjs/toolkit'
 import blogService from './blogService';
 
 
 export const getBlogs = createAsyncThunk('blog/get-blogs', async (thunkAPI) => {
-    try {
-        return await blogService.getBlogs();
-    } catch (error) {
-        return thunkAPI.rejectWithValue(error)
-    }
+   try {
+      return await blogService.getBlogs();
+   } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+   }
 })
 
 export const createBlog = createAsyncThunk('blog/create-blog', async (blogData, thunkAPI) => {
-    try {
-        return await blogService.createBlog(blogData);
-    } catch (error) {
-        return thunkAPI.rejectWithValue(error)
-    }
+   try {
+      return await blogService.createBlog(blogData);
+   } catch (error) {
+      return thunkAPI.rejectWithValue(error)
+   }
 })
 
+export const resetState = createAction('Reset_all')
+
 const initialState = {
-    blogs: [],
-    isError: false,
-    isLoading: false,
-    isSuccess: false,
-    message: "",
+   blogs: [],
+   isError: false,
+   isLoading: false,
+   isSuccess: false,
+   message: "",
 }
 
 export const blogSlice = createSlice({
-    name: "blogs",
-    initialState,
-    reducer: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(getBlogs.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(getBlogs.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.isError = false;
-                state.isSuccess = true;
-                state.blogs = action.payload;
-            })
-            .addCase(getBlogs.rejected, (state, action) => {
-                state.isLoading = false;
-                state.isError = true;
-                state.isSuccess = false;
-                state.message = action.error;
-            })
-            .addCase(createBlog.pending, (state) => {
-                state.isLoading = true;
-            })
-            .addCase(createBlog.fulfilled, (state, action) => {
-                state.isLoading = false;
-                state.isError = false;
-                state.isSuccess = true;
-                state.createdBlog = action.payload;
-            })
-            .addCase(createBlog.rejected, (state, action) => {
-                state.isLoading = false;
-                state.isError = true;
-                state.isSuccess = false;
-                state.message = action.error;
-            })
-    }
+   name: "blogs",
+   initialState,
+   reducer: {},
+   extraReducers: (builder) => {
+      builder
+         .addCase(getBlogs.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(getBlogs.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.blogs = action.payload;
+         })
+         .addCase(getBlogs.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.isSuccess = false;
+            state.message = action.error;
+         })
+         .addCase(createBlog.pending, (state) => {
+            state.isLoading = true;
+         })
+         .addCase(createBlog.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.createdBlog = action.payload;
+         })
+         .addCase(createBlog.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.isSuccess = false;
+            state.message = action.error;
+         })
+         .addCase(resetState, () => initialState)
+   }
 })
 
 export default blogSlice.reducer
